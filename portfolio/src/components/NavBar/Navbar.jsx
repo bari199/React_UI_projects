9043dfc21ae4167537acc0c5f5ac022d4ef57624
bari@199 <bari1999smab@gmail.com>
2025-09-26 11:23:27 -0700

    added components

---
+++ portfolio/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './navbar.css';
+import logo from '../../assets/log.png';
+import contactImg from '../../assets/contactImg.png'
+import {Link} from 'react-scroll';
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <img src={logo} alt="logo" className='logo' />
+        <div className="desktopMenu">
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className="desktopMenuListItem">Abouts</Link>
+            <Link className="desktopMenuListItem">Portfolio</Link>
+            <Link className="desktopMenuListItem">Clients</Link>
+        </div>
+        <button className="desktopMenuBtn">
+            <img src={contactImg} alt="contactImg" className="desktopMenuImg" />Contacts
+        </button>
+
+    </nav>
+  )
+}
+
+export default Navbar
